1293ef73d0d940c5504eb37c88baa511e4233e53
kyohiro <kyohiro@gmail.com>
2014-01-03 23:15:09 +0800

    task should be serializable

--- src/main/scala/akkaworker/task/Task.scala
+++ src/main/scala/akkaworker/task/Task.scala
@@ -2,7 +2,7 @@ package akkaworker.task
 
 import scala.concurrent.Future
 
-trait Task {
+trait Task extends Serializable {
   val id: Long  //A Task should have any unique id
   def workOnTask: Future[Option[Any]]  
 }
